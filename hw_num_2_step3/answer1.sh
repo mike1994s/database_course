b1d653646455400f0299fcbce45b82257ebcdc3a
Misha <meshuin94@gmail.com>
2016-07-01 12:11:05 +0400

    fix

--- hw_num_2_step3/answer1.sh
+++ hw_num_2_step3/answer1.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #sqlite3 m3_hw2.db "SELECT * FROM Conference"
 
-sqlite3 m3_hw2.db "SELECT conference, COUNT(conference) FROM
+sqlite3 m3_hw2.db "SELECT conference FROM
 (SELECT conference,location FROM Paper GROUP BY conference, location) AS InTable GROUP BY conference HAVING COUNT(conference) > 1 ORDER BY conference ASC"
-
 
 #sqlite3 m3_hw2.db "SELECT title FROM Paper WHERE conference NOT IN (SELECT value FROM Conference) AS InTable;"
 
